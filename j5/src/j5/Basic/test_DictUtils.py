85213f09308743fec9b07d62def59f667ca993ee
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2005-09-30 09:57:03 +0000

    put test into class, added one for remembering original case
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@10 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/test_DictUtils.py
+++ j5/src/j5/Basic/test_DictUtils.py
@@ -2,8 +2,17 @@
 import Namespace
 from Basic import DictUtils
 
-def test_cikeys():
-    d = DictUtils.cidict()
-    d["VaLuE"] = 5
-    assert d["value"] == 5
+class TestCIDict:
+    def test_get(self):
+        d = DictUtils.cidict()
+        d["VaLuE"] = 5
+        assert d["value"] == 5
+
+    def test_retaincase(self):
+        """tests that the key's original case is retained"""
+        d = DictUtils.cidict()
+        d["VaLuE"] = 5
+        assert "VaLuE" in d.keys()
+        assert "value" not in d.keys()
+
     
