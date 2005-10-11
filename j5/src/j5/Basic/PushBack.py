71bb790cb62e3ba18e459c64c34073ec7acacd65
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2005-10-11 13:42:51 +0000

    added simple pushback class for putting things back into an iterator
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@56 25043fe6-1616-0410-a82d-ba8f324cfe61

---
+++ j5/src/j5/Basic/PushBack.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""this is an iterator wrapper that allows things to be pushed back"""
+
+class PushBack(object):
+  def __init__(self, iterator):
+    self.iterator = iterator
+    self.pushedback = []
+  def __iter__(self):
+    return self
+  def pushback(self, item):
+    self.pushedback.append(item)
+  def next(self):
+    if self.pushedback:
+      return self.pushedback.pop(0)
+    else:
+      return self.iterator.next()
+    
---
+++ j5/src/j5/Basic/test_PushBack.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from Basic import PushBack
+
+class TestPushBack:
+    def test_wrap(self):
+        i = [1,2,3,4,5]
+        g = PushBack.PushBack(i.__iter__())
+        l = list(g)
+        assert i == l
+    def test_simplepush(self):
+        i = [1,2,3,4,5]
+        g = PushBack.PushBack(i.__iter__())
+        g.pushback(6)
+        l = list(g)
+        assert l == [6,1,2,3,4,5]
+    def test_multipush(self):
+        i = [1,2,3,4,5]
+        g = PushBack.PushBack(i.__iter__())
+        g.pushback(6)
+        assert g.next() == 6
+        assert g.next() == 1
+        g.pushback(7)
+        assert g.next() == 7
+        l = list(g)
+        assert l == [2,3,4,5]
+        g.pushback(8)
+        l = list(g)
+        assert l == [8]
+        l = list(g)
+        assert l == []
+
