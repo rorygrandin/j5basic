47a1964066c9648c0c1aeee3a7365cba4a7844c0
simonc <simonc@25043fe6-1616-0410-a82d-ba8f324cfe61>
2006-05-26 11:59:50 +0000

    Tidy up Decorators.py and test_Decorators.py. Add SelfLocking decorator and a test for it.
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@1157 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/Decorators.py
+++ j5/src/j5/Basic/Decorators.py
@@ -1,143 +1,26 @@
-## The basic trick is to generate the source code for a lambda function
-## with the right signature and to evaluate it.
-## Uncomment the 'print lambda_src' statement in _decorate
-## to understand what is going on.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__all__ = ["decorator", "copyfunc", "getinfo"]
+"""Various decorators"""
+
+# Copyright 2006 St James Software
+
+__all__ = ["decorator","SelfLocking"]
 
 import inspect, new, itertools
 
-def getinfo(func, extendedargs=[]):
-    """Return an info dictionary containing:
-    - name (the name of the function : str)
-    - argnames (the names of the arguments : list)
-    - defarg (the values of the default arguments : list)
-    - fullsign (the full signature : str)
-    - shortsign (the short signature : str)
-    - arg0 ... argn (shortcuts for the names of the arguments)
-    """
-    assert inspect.ismethod(func) or inspect.isfunction(func) or isinstance(func, classmethod)
-    regargs, varargs, varkwargs, defaults = inspect.getargspec(func)
-    if extendedargs:
-        if defaults is None:
-            defaults = []
-        else:
-            defaults = list(defaults)
-        has_defaults = len(defaults) > 0
-        for extendedarg in extendedargs:
-            has_argdefault, argdefault = False, None
-            if isinstance(extendedarg, tuple) and len(extendedarg) == 2:
-                argname, argdefault = extendedarg
-                has_argdefault = True
-            elif isinstance(extendedarg, tuple) and len(extendedarg) == 1:
-                argname = extendedarg[0]
-            elif isinstance(extendedarg, basestring):
-                argname = extendedarg
-            else:
-                raise ValueError("Extended argument should be (keyword, default), (keyword,), or just keyword: got %r" % extendedarg)
-            if has_defaults and not has_argdefault:
-                raise ValueError("Trying to add extended argument without defaults after arguments with defaults: %r" % (extendedarg,))
-            existing_arg = argname in regargs
-            if not existing_arg:
-                regargs.append(argname)
-            if has_argdefault:
-                if existing_arg:
-                    arg_pos = regargs.index(argname)
-                    defaults_pos = len(regargs) - len(defaults)
-                    # this can handle altering existing default values,
-                    # or setting the default value for the last non-default value
-                    # a number of non-default values can thus be set provided the order is right...
-                    if defaults_pos <= arg_pos:
-                        defaults[arg_pos - defaults_pos] = argdefault
-                    elif defaults_pos == arg_pos + 1:
-                        defaults.insert(0, argdefault)
-                    else:
-                        raise ValueError("Couldn't add default to existing argument %s" % argname)
-                else:
-                    defaults.append(argdefault)
-                has_defaults = True
-        defaults = tuple(defaults)
-    argnames = list(regargs)
-    if varargs: argnames.append(varargs)
-    if varkwargs: argnames.append(varkwargs)
-    counter = itertools.count()
-    fullsign = inspect.formatargspec(
-        regargs, varargs, varkwargs, defaults,
-        formatvalue=lambda value: "=defarg[%i]" % counter.next())[1:-1]
-    shortsign = inspect.formatargspec(
-        regargs, varargs, varkwargs, defaults,
-        formatvalue=lambda value: "")[1:-1]
-    dic = dict(("arg%s" % n, name) for n, name in enumerate(argnames))
-    dic.update(name=func.__name__, argnames=argnames, shortsign=shortsign,
-        fullsign = fullsign, defarg = defaults or ())
-    return dic
-
-def _contains_reserved_names(dic): # helper
-    return "_call_" in dic or "_func_" in dic
-
-def _decorate(func, caller, extendedargs={}):
-    """Takes a function and a caller and returns the function
-    decorated with that caller. The decorated function is obtained
-    by evaluating a lambda function with the correct signature.
-    """
-    infodict = getinfo(func, extendedargs)
-    defaults = infodict["defarg"]
-    assert not _contains_reserved_names(infodict["argnames"]), \
-           "You cannot use _call_ or _func_ as argument names!"
-    execdict = dict(_func_=func, _call_=caller, defarg=defaults or ())
-    if func.__name__ == "<lambda>":
-        lambda_src = "lambda %(fullsign)s: _call_(_func_, %(shortsign)s)" \
-                     % infodict
-        func_code = compile(lambda_src, func.func_code.co_filename, 'eval')
-    else:
-        func_src = """def %(name)s(%(fullsign)s):
-        return _call_(_func_, %(shortsign)s)""" % infodict
-        func_code = compile(func_src, func.func_code.co_filename, 'exec')
-    func_internal_code = func_code.co_consts[len(defaults or ())]
-    dec_func = new.function(func_internal_code, execdict, func.__name__, defaults)
-    #import sys; print >> sys.stderr, func_src # for debugging 
-    dec_func.__doc__ = func.__doc__
-    dec_func.__dict__ = func.__dict__
-    return dec_func
-
-class decorator(object):
-    """General purpose decorator factory: takes a caller function as
-input and returns a decorator. extendedargs, if given is a list of
-keyword arguments and defaults that will be added to the function signature
-A caller function is any function like this::
-
-    def caller(func, *args, **kw):
-        # do something
-        return func(*args, **kw)
-    
-Here is an example of usage:
-
-    >>> @decorator
-    ... def chatty(f, *args, **kw):
-    ...     print "Calling %r" % f.__name__
-    ...     return f(*args, **kw)
-    
-    >>> @chatty
-    ... def g(): pass
-    ...
-    >>> g()
-    Calling 'g'
-    """
-    def __init__(self, caller, extendedargs=[]):
-        self.caller = caller
-        self.extendedargs = extendedargs
-    def __call__(self, func):
-        return _decorate(func, self.caller, self.extendedargs)
+#
+# Utility Functions (for working with other functions)
+#
 
 def copyfunc(func): # not used internally
     "Creates an independent copy of a function."
     return new.function(func.func_code, func.func_globals, func.func_name,
                         func.func_defaults, func.func_closure)
 
-# FIXME: Not sure this should be here
 def getrightargs(function, args):
     """Returns a dictionary of only the arguments which the callable takes out of the args.
-    args is a dictionary as one might receive from accepting **kwargs for a function"""
+       args is a dictionary as one might receive from accepting **kwargs for a function"""
     if not (inspect.isfunction(function) or inspect.ismethod(function)):
         # Try to find a function
         if not inspect.isclass(function):
@@ -158,3 +41,162 @@ def getrightargs(function, args):
         return newdict
     return args
 
+
+#
+# decorator decorator (for making other decorators)
+#
+
+class decorator_helpers(object):
+    """The basic trick is to generate the source code for a lambda function
+       with the right signature and to evaluate it.
+       Print lambda_src in _decorate to understand what is going on."""
+
+    @staticmethod
+    def getinfo(func, extendedargs=[]):
+        """Return an info dictionary containing:
+           - name (the name of the function : str)
+           - argnames (the names of the arguments : list)
+           - defarg (the values of the default arguments : list)
+           - fullsign (the full signature : str)
+           - shortsign (the short signature : str)
+           - arg0 ... argn (shortcuts for the names of the arguments)"""
+
+        assert inspect.ismethod(func) or inspect.isfunction(func) or isinstance(func, classmethod)
+        regargs, varargs, varkwargs, defaults = inspect.getargspec(func)
+        if extendedargs:
+            if defaults is None:
+                defaults = []
+            else:
+                defaults = list(defaults)
+            has_defaults = len(defaults) > 0
+            for extendedarg in extendedargs:
+                has_argdefault, argdefault = False, None
+                if isinstance(extendedarg, tuple) and len(extendedarg) == 2:
+                    argname, argdefault = extendedarg
+                    has_argdefault = True
+                elif isinstance(extendedarg, tuple) and len(extendedarg) == 1:
+                    argname = extendedarg[0]
+                elif isinstance(extendedarg, basestring):
+                    argname = extendedarg
+                else:
+                    raise ValueError("Extended argument should be (keyword, default), (keyword,), or just keyword: got %r" % extendedarg)
+                if has_defaults and not has_argdefault:
+                    raise ValueError("Trying to add extended argument without defaults after arguments with defaults: %r" % (extendedarg,))
+                existing_arg = argname in regargs
+                if not existing_arg:
+                    regargs.append(argname)
+                if has_argdefault:
+                    if existing_arg:
+                        arg_pos = regargs.index(argname)
+                        defaults_pos = len(regargs) - len(defaults)
+                        # this can handle altering existing default values,
+                        # or setting the default value for the last non-default value
+                        # a number of non-default values can thus be set provided the order is right...
+                        if defaults_pos <= arg_pos:
+                            defaults[arg_pos - defaults_pos] = argdefault
+                        elif defaults_pos == arg_pos + 1:
+                            defaults.insert(0, argdefault)
+                        else:
+                            raise ValueError("Couldn't add default to existing argument %s" % argname)
+                    else:
+                        defaults.append(argdefault)
+                    has_defaults = True
+            defaults = tuple(defaults)
+        argnames = list(regargs)
+        if varargs: argnames.append(varargs)
+        if varkwargs: argnames.append(varkwargs)
+        counter = itertools.count()
+        fullsign = inspect.formatargspec(
+            regargs, varargs, varkwargs, defaults,
+            formatvalue=lambda value: "=defarg[%i]" % counter.next())[1:-1]
+        shortsign = inspect.formatargspec(
+            regargs, varargs, varkwargs, defaults,
+            formatvalue=lambda value: "")[1:-1]
+        dic = dict(("arg%s" % n, name) for n, name in enumerate(argnames))
+        dic.update(name=func.__name__, argnames=argnames, shortsign=shortsign,
+            fullsign = fullsign, defarg = defaults or ())
+        return dic
+
+    @staticmethod
+    def _contains_reserved_names(dic):
+        return "_call_" in dic or "_func_" in dic
+
+    @staticmethod
+    def _decorate(func, caller, extendedargs={}):
+        """Takes a function and a caller and returns the function
+           decorated with that caller. The decorated function is obtained
+           by evaluating a lambda function with the correct signature."""
+        infodict = decorator_helpers.getinfo(func, extendedargs)
+        defaults = infodict["defarg"]
+        assert not decorator_helpers._contains_reserved_names(infodict["argnames"]), \
+               "You cannot use _call_ or _func_ as argument names!"
+        execdict = dict(_func_=func, _call_=caller, defarg=defaults or ())
+        if func.__name__ == "<lambda>":
+            lambda_src = "lambda %(fullsign)s: _call_(_func_, %(shortsign)s)" \
+                         % infodict
+            func_code = compile(lambda_src, func.func_code.co_filename, 'eval')
+        else:
+            func_src = """def %(name)s(%(fullsign)s):
+            return _call_(_func_, %(shortsign)s)""" % infodict
+            func_code = compile(func_src, func.func_code.co_filename, 'exec')
+        func_internal_code = func_code.co_consts[len(defaults or ())]
+        dec_func = new.function(func_internal_code, execdict, func.__name__, defaults)
+        dec_func.__doc__ = func.__doc__
+        dec_func.__dict__ = func.__dict__
+        return dec_func
+
+class decorator(object):
+    """General purpose decorator factory: takes a caller function as
+       input and returns a decorator. extendedargs, if given is a list of
+       keyword arguments and defaults that will be added to the function signature
+       A caller function is any function like this::
+
+       def caller(func, *args, **kw):
+           # do something
+           return func(*args, **kw)
+    
+       Here is an example of usage:
+
+           >>> @decorator
+           ... def chatty(f, *args, **kw):
+           ...     print "Calling %r" % f.__name__
+           ...     return f(*args, **kw)
+    
+           >>> @chatty
+           ... def g(): pass
+           ...
+           >>> g()
+           Calling 'g'"""
+
+    def __init__(self, caller, extendedargs=[]):
+        self.caller = caller
+        self.extendedargs = extendedargs
+
+    def __call__(self, func):
+        return decorator_helpers._decorate(func, self.caller, self.extendedargs)
+
+
+#
+# Decorators for Self Locking objects.
+#
+
+class SelfLocking(object):
+    @staticmethod
+    def runwithlock(f):
+        """Can only be used on objects which have a self.lock (class or instance)
+           attribute which supports the acquire() and release() methods.
+           The decorator acquires self.lock, runs the method and releases the lock
+           afterwards."""
+
+        def wrapper(self,*args,**kws):
+            try:
+                self.lock.acquire()
+                ret = f(self,*args,**kws)
+            finally:
+                self.lock.release()
+            return ret
+
+        wrapper.__doc__ = f.__doc__
+        wrapper.func_name = f.func_name
+
+        return wrapper
--- j5/src/j5/Basic/test_Decorators.py
+++ j5/src/j5/Basic/test_Decorators.py
@@ -1,123 +1,161 @@
 #!/usr/bin/env python
 
 from j5.Basic import Decorators
+import threading
+import time
 
-def f(self, x=1, y=2, *args, **kw):
-    """sample function for testing signatures etc"""
-    pass
+class TestDecoratorDecorator(object):
 
-def chatty(f, *args, **kw):
-    """sample decorator that logs calls on f.calls"""
-    if not hasattr(f, "calls"):
-        f.calls = []
-    f.calls.append("Calling %r" % f.__name__)
-    return f(*args, **kw)
+    @staticmethod
+    def f(self, x=1, y=2, *args, **kw):
+        """sample function for testing signatures etc"""
+        pass
 
-def extdec(f, x, y, z):
-    """decorator extending underlying function"""
-    if not hasattr(f, "calls"):
-        f.calls = []
-    f.calls.append("Called with x=%r, y=%r, z=%r" % (x, y, z))
-    return f(x + y * z)
+    @staticmethod
+    def chatty(f, *args, **kw):
+        """sample decorator that logs calls on f.calls"""
+        if not hasattr(f, "calls"):
+            f.calls = []
+        f.calls.append("Calling %r" % f.__name__)
+        return f(*args, **kw)
 
-def g(x):
-    """returns x plus 25"""
-    return x + 25
+    @staticmethod
+    def extdec(f, x, y, z):
+        """decorator extending underlying function"""
+        if not hasattr(f, "calls"):
+            f.calls = []
+        f.calls.append("Called with x=%r, y=%r, z=%r" % (x, y, z))
+        return f(x + y * z)
 
-def extdec2(f, *args, **kw):
-    """decorator extending underlying function, and using args and kwargs"""
-    if not hasattr(f, "calls"):
-        f.calls = []
-    args = list(args)
-    def poparg(argname):
-        if argname in kw:
-            return kw.pop(argname)
-        else:
-            return args.pop()
-    y = poparg("y")
-    z = poparg("z")
-    x = poparg("x")
-    f.calls.append("Called with x=%r, y=%r, z=%r" % (x, y, z))
-    return f(x + y * z, z)
+    @staticmethod
+    def g(x):
+        """returns x plus 25"""
+        return x + 25
 
-def g2(x, z=4):
-    """returns x plus z plus 25"""
-    return x + z + 25
+    @staticmethod
+    def extdec2(f, *args, **kw):
+        """decorator extending underlying function, and using args and kwargs"""
+        if not hasattr(f, "calls"):
+            f.calls = []
+        args = list(args)
+        def poparg(argname):
+            if argname in kw:
+                return kw.pop(argname)
+            else:
+                return args.pop()
+        y = poparg("y")
+        z = poparg("z")
+        x = poparg("x")
+        f.calls.append("Called with x=%r, y=%r, z=%r" % (x, y, z))
+        return f(x + y * z, z)
 
-def test_getinfo():
-    """tests that the getinfo function returns the correct information about a function signature"""
-    info = Decorators.getinfo(f)
-    assert info["name"] == 'f'
-    assert info["argnames"] == ['self', 'x', 'y', 'args', 'kw']
-    assert info["defarg"] == (1, 2)
-    assert info["shortsign"] == 'self, x, y, *args, **kw'
-    assert info["fullsign"] == 'self, x=defarg[0], y=defarg[1], *args, **kw'
-    assert (info["arg0"], info["arg1"], info["arg2"], info["arg3"], info["arg4"]) == ('self', 'x', 'y', 'args', 'kw')
+    @staticmethod
+    def g2(x, z=4):
+        """returns x plus z plus 25"""
+        return x + z + 25
 
-def test_decorator():
-    """test that a decorator is called, and properly calls the underlying function"""
-    chatty_decorator = Decorators.decorator(chatty)
-    chatty_g = chatty_decorator(g)
-    result = chatty_g(3)
-    assert result == 28
-    assert g.calls[-1] == "Calling 'g'"
+    def test_getinfo(self):
+        """tests that the getinfo function returns the correct information about a function signature"""
+        info = Decorators.decorator_helpers.getinfo(self.f)
+        assert info["name"] == 'f'
+        assert info["argnames"] == ['self', 'x', 'y', 'args', 'kw']
+        assert info["defarg"] == (1, 2)
+        assert info["shortsign"] == 'self, x, y, *args, **kw'
+        assert info["fullsign"] == 'self, x=defarg[0], y=defarg[1], *args, **kw'
+        assert (info["arg0"], info["arg1"], info["arg2"], info["arg3"], info["arg4"]) == ('self', 'x', 'y', 'args', 'kw')
 
-def test_decorator_lambda():
-    """test that a decorator is called, and properly calls the underlying function"""
-    l = lambda x: x + 21
-    assert l(3) == 24
-    chatty_decorator = Decorators.decorator(chatty)
-    chatty_l = chatty_decorator(l)
-    assert chatty_l(3) == 24
+    def test_decorator(self):
+        """test that a decorator is called, and properly calls the underlying function"""
+        chatty_decorator = Decorators.decorator(self.chatty)
+        chatty_g = chatty_decorator(self.g)
+        result = chatty_g(3)
+        assert result == 28
+        assert self.g.calls[-1] == "Calling 'g'"
 
-def test_decorator_signature():
-    """tests that a decorated function retains the signature of the underlying function"""
-    chatty_decorator = Decorators.decorator(chatty)
-    chatty_f = chatty_decorator(f)
-    info = Decorators.getinfo(f)
-    chatty_info = Decorators.getinfo(chatty_f)
-    assert info == chatty_info
-    assert info.__doc__ == chatty_info.__doc__
+    def test_decorator_lambda(self):
+        """test that a decorator is called, and properly calls the underlying function"""
+        l = lambda x: x + 21
+        assert l(3) == 24
+        chatty_decorator = Decorators.decorator(self.chatty)
+        chatty_l = chatty_decorator(l)
+        assert chatty_l(3) == 24
 
-def test_extend_decorator():
-    """tests that a decorated function can extend the underlying function"""
-    ext_decorator = Decorators.decorator(extdec, ['y', ('z', 3)])
-    ext_g = ext_decorator(g)
-    assert ext_g(100, 4) == 100 + 12 + 25
-    assert g.calls[-1] == "Called with x=100, y=4, z=3"
+    def test_decorator_signature(self):
+        """tests that a decorated function retains the signature of the underlying function"""
+        chatty_decorator = Decorators.decorator(self.chatty)
+        chatty_f = chatty_decorator(self.f)
+        info = Decorators.decorator_helpers.getinfo(self.f)
+        chatty_info = Decorators.decorator_helpers.getinfo(chatty_f)
+        assert info == chatty_info
+        assert info.__doc__ == chatty_info.__doc__
 
-def test_extend_decorator_signature():
-    """tests that a decorated function can extend the signature of the underlying function"""
-    ext_decorator = Decorators.decorator(extdec, ['y', ('z', 3)])
-    ext_g = ext_decorator(g)
-    info = Decorators.getinfo(g)
-    ext_info = Decorators.getinfo(ext_g)
-    assert ext_info.__doc__ == info.__doc__
-    assert ext_info["name"] == info["name"]
-    assert ext_info["argnames"] == ['x', 'y', 'z']
-    assert ext_info["defarg"] == (3,)
-    assert ext_info["shortsign"] == 'x, y, z'
-    assert ext_info["fullsign"] == 'x, y, z=defarg[0]'
-    assert ext_info["arg0"] == 'x'
-    assert ext_info["arg1"] == 'y'
-    assert ext_info["arg2"] == 'z'
+    def test_extend_decorator(self):
+        """tests that a decorated function can extend the underlying function"""
+        ext_decorator = Decorators.decorator(self.extdec, ['y', ('z', 3)])
+        ext_g = ext_decorator(self.g)
+        assert ext_g(100, 4) == 100 + 12 + 25
+        assert self.g.calls[-1] == "Called with x=100, y=4, z=3"
 
-def test_extend_decorator_existing():
-    """tests that a decorated function can extend the underlying function, handling existing arguments right"""
-    ext_decorator = Decorators.decorator(extdec2, [('y', 5), ('z', 3), ('x', 0)])
-    ext_g = ext_decorator(g2)
-    info = Decorators.getinfo(g2)
-    ext_info = Decorators.getinfo(ext_g)
-    assert ext_info.__doc__ == info.__doc__
-    assert ext_info["name"] == info["name"]
-    assert ext_info["argnames"] == ['x', 'z', 'y']
-    assert ext_info["defarg"] == (0, 3, 5)
-    assert ext_info["shortsign"] == 'x, z, y'
-    assert ext_info["fullsign"] == 'x=defarg[0], z=defarg[1], y=defarg[2]'
-    assert ext_info["arg0"] == 'x'
-    assert ext_info["arg1"] == 'z'
-    assert ext_info["arg2"] == 'y'
-    result = ext_g()
-    assert result == (0 + 3*5) + 3 + 25
-    assert g2.calls[-1] == "Called with x=0, y=5, z=3"
+    def test_extend_decorator_signature(self):
+        """tests that a decorated function can extend the signature of the underlying function"""
+        ext_decorator = Decorators.decorator(self.extdec, ['y', ('z', 3)])
+        ext_g = ext_decorator(self.g)
+        info = Decorators.decorator_helpers.getinfo(self.g)
+        ext_info = Decorators.decorator_helpers.getinfo(ext_g)
+        assert ext_info.__doc__ == info.__doc__
+        assert ext_info["name"] == info["name"]
+        assert ext_info["argnames"] == ['x', 'y', 'z']
+        assert ext_info["defarg"] == (3,)
+        assert ext_info["shortsign"] == 'x, y, z'
+        assert ext_info["fullsign"] == 'x, y, z=defarg[0]'
+        assert ext_info["arg0"] == 'x'
+        assert ext_info["arg1"] == 'y'
+        assert ext_info["arg2"] == 'z'
 
+    def test_extend_decorator_existing(self):
+        """tests that a decorated function can extend the underlying function, handling existing arguments right"""
+        ext_decorator = Decorators.decorator(self.extdec2, [('y', 5), ('z', 3), ('x', 0)])
+        ext_g = ext_decorator(self.g2)
+        info = Decorators.decorator_helpers.getinfo(self.g2)
+        ext_info = Decorators.decorator_helpers.getinfo(ext_g)
+        assert ext_info.__doc__ == info.__doc__
+        assert ext_info["name"] == info["name"]
+        assert ext_info["argnames"] == ['x', 'z', 'y']
+        assert ext_info["defarg"] == (0, 3, 5)
+        assert ext_info["shortsign"] == 'x, z, y'
+        assert ext_info["fullsign"] == 'x=defarg[0], z=defarg[1], y=defarg[2]'
+        assert ext_info["arg0"] == 'x'
+        assert ext_info["arg1"] == 'z'
+        assert ext_info["arg2"] == 'y'
+        result = ext_g()
+        assert result == (0 + 3*5) + 3 + 25
+        assert self.g2.calls[-1] == "Called with x=0, y=5, z=3"
+
+class TestSelfLocking(object):
+
+    def test_foo(self):
+        THREADS = 4
+
+        class Foo(object):
+            lock = threading.Lock()
+            res = []
+
+            @Decorators.SelfLocking.runwithlock
+            def haslock(self,i):
+                self.res.append(i)
+                time.sleep(0.5)
+                self.res.append(i)
+
+        threads = []
+        for i in range(THREADS):
+            threads.append(threading.Thread(target=Foo().haslock,args=[i]))
+
+        for thrd in threads:
+            thrd.start()
+
+        for thrd in threads:
+            thrd.join()
+
+        assert len(Foo.res) == 2*THREADS
+        for i in range(THREADS):
+            assert Foo.res[2*i] == Foo.res[2*i+1]
