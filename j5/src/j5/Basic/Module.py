a31b9559f510022284feffd45f052f764bcb1c24
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2006-12-07 10:54:25 +0000

    pass loglevel down into getimportablemodule (avoid warnings on test imports) remove copied comment that didn't apply remove old getimportablemodule code now that I know the new code is better
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@3461 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/Module.py
+++ j5/src/j5/Basic/Module.py
@@ -15,7 +15,7 @@ def resolvemodule(modulename, loglevel=logging.WARN):
         return importedmodules[modulename]
 
     try:
-        parentmodule = getimportablemodule(modulename)
+        parentmodule = getimportablemodule(modulename, loglevel)
     except (ImportError, SyntaxError), e:
         logging.log(loglevel, "Could not import module for %s" % (modulename))
         raise AttributeError(str(e))
@@ -37,7 +37,7 @@ thisfilename = canonicalize(__file__)
 if thisfilename.endswith(".pyc") or thisfilename.endswith(".pyo"):
     thisfilename = thisfilename[:-1]
 
-def getimportablemodule(modulename):
+def getimportablemodule(modulename, loglevel=logging.WARN):
     """Attempts to import successive modules on the a.b.c route - first a.b.c, then a.b, etc. Only goes one level up"""
     components = modulename.split('.')
     module = None
@@ -49,7 +49,7 @@ def getimportablemodule(modulename):
             parentmodule = __import__(parentmodulename)
         except ImportError, error:
             # if we get an import error on the parent module, we're unlikely to be able to import the child
-            logging.warning("Import Error attempting to import %s (parent of %s): %s" % (parentmodulename, modulename, error))
+            logging.log(loglevel, "Import Error attempting to import %s (parent of %s): %s" % (parentmodulename, modulename, error))
             raise
         except Exception, error:
             logging.debug("Error attempting to import %s: %s" % (parentmodulename, error))
@@ -58,7 +58,6 @@ def getimportablemodule(modulename):
         module = __import__(modulename)
         return module
     except ImportError, error:
-        # if we get an import error on the parent module, we're unlikely to be able to import the child
         logging.debug("Import Error attempting to import %s (but have parent module to return): %s" % (modulename, error))
         if component_depth > 1:
             return parentmodule
@@ -66,32 +65,6 @@ def getimportablemodule(modulename):
     except Exception, error:
         logging.debug("Error attempting to import %s: %s" % (modulename, error))
         raise
-    # FIXME: clear out the code below - the above should be clearer and simpler and not cause problems...
-
-    while currentattempt > 0:
-        try:
-            attemptedname = '.'.join(components[:currentattempt])
-            module = __import__(attemptedname)
-            return module
-        except ImportError, error:
-            # if the ImportError originated from outside this file then raise it, otherwise continue
-            cls, exc, trc = sys.exc_info()
-            filename, line_number, function_name, text = traceback.extract_tb(trc, 10)[-1]
-            if filename.endswith(".pyc") or filename.endswith(".pyo"):
-                filename = filename[:-1]
-            # need to compare to the canonical version of this filename - and make it case insensitive for windows drive letters
-            # FIXME: this assumes that the relative path is relative to the current directory, which fails under py2exe with .pyc files if run from another directory
-            if canonicalize(filename).lower() != thisfilename.lower():
-                logging.warning("Import Error attempting to import %s (%s), comes from file %s which seems to be a real module that can't be imported" % (attemptedname, error, filename))
-                logging.debug("ImportError came from filename %s, this file is %s" % (filename, thisfilename))
-                raise
-            logging.debug("Import Error attempting to import %s: %s" % (attemptedname, error))
-            currentattempt -= 1
-            errormessage = error
-        except Exception, error:
-            logging.debug("Error attempting to import %s: %s" % (attemptedname, error))
-            raise
-    raise ImportError("Error importing module %r: %s\nPython path is %r" % (modulename, errormessage, sys.path))
 
 def getpart(module, partname):
     components = partname.split('.')
