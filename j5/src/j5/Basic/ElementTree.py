ab4804e728fc02c4cb7132d4ac01574bc8779f1e
simonc <simonc@25043fe6-1616-0410-a82d-ba8f324cfe61>
2007-05-04 07:25:45 +0000

    When importing cElementTree, import a few extra goodies (needed by XMLStorage) from ElementTree.
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@4086 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/ElementTree.py
+++ j5/src/j5/Basic/ElementTree.py
@@ -1,35 +1,50 @@
 """Wrapper for importing any of the various ElementTree implementations."""
 
-ElementTreeImported = False
+SomeElementTreeImported = False
+cElementTreeImported = False
 
-if not ElementTreeImported:
+# Import Basic ElementTree
+
+if not SomeElementTreeImported:
     try:
         # default location of cElementTree on Python >= 2.5
         from xml.etree.cElementTree import *
-        ElementTreeImported = True
+        SomeElementTreeImported = True
+        cElementTreeImported = True
     except ImportError:
         pass
 
-if not ElementTreeImported:
+if not SomeElementTreeImported:
     try:
         # default location of ElementTree on Python >= 2.5
         from xml.etree.ElementTree import *
-        ElementTreeImported = True
+        SomeElementTreeImported = True
     except ImportError:
         pass
 
-if not ElementTreeImported:
+if not SomeElementTreeImported:
     try:
         # try standalone cElementTree install
         from cElementTree import *
-        ElementTreeImported = True
+        SomeElementTreeImported = True
+        cElementTreeImported = True
     except ImportError:
         pass
 
-if not ElementTreeImported:
+if not SomeElementTreeImported:
     # last ditch attempt at import
     # try standalone ElementTree install
-    from elementtree import *
-    ElementTreeImported = True
+    from elementtree.ElementTree import *
+    SomeElementTreeImported = True
 
+# Import Extra Things from ElementTree if cElementTree Used
 
+if cElementTreeImported:
+    try:
+        # Try import extras from default location of ElementTree in Python >= 2.5
+        from xml.etree.ElementTree import _escape_cdata, _raise_serialization_error, \
+                                          _encode, _escape_attrib, _encode_entity
+    except ImportError:
+        # Try import extras from standalone ElementTree install
+        from elementtree.ElementTree import _escape_cdata, _raise_serialization_error, \
+                                            _encode, _escape_attrib, _encode_entity
