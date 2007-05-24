41f84093df61d2ad5e4af9dbd681c586dcf0cd15
simonc <simonc@25043fe6-1616-0410-a82d-ba8f324cfe61>
2007-05-24 09:18:23 +0000

    Add id generator which prefixes unique IDs with ID (this is mainly for storing IDs in SQLite, our testing database, which thinks any string of digits is a number. Add tests for Uid methods.
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@4160 25043fe6-1616-0410-a82d-ba8f324cfe61

---
+++ j5/src/j5/Basic/test_Uid.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from j5.Basic import Uid
+import re
+
+class TestUid(object):
+    def test_uid_digit_str(self):
+        ids = {}
+        for i in range(10000):
+            ids[Uid.uid_digit_str()] = None
+
+        assert len(ids) == 10000 # no duplicates
+
+        # check format
+        uid_re = re.compile("[0-9]{17,22}")
+        for uid in ids:
+            assert uid_re.match(uid)
+
+    def test_uid_id_str(self):
+        ids = {}
+        for i in range(10000):
+            ids[Uid.uid_id_str()] = None
+
+        assert len(ids) == 10000 # no duplicates
+
+        # check format
+        uid_re = re.compile("ID[0-9]{17,22}")
+        for uid in ids:
+            assert uid_re.match(uid)
