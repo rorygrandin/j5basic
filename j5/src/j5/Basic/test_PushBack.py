d58bc5cd2151cd46019322369a37bd82b16f7a8f
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-03-14 14:39:56 +0000

    Add PushToBack iterator under PushBack, which can push values to the end of a queue while iterating Added tests for it
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@5387 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/test_PushBack.py
+++ j5/src/j5/Basic/test_PushBack.py
@@ -30,3 +30,32 @@ class TestPushBack:
         l = list(g)
         assert l == []
 
+class TestPushToBack:
+    def test_wrap(self):
+        i = [1,2,3,4,5]
+        g = PushBack.PushToBack(i.__iter__())
+        l = list(g)
+        assert i == l
+
+    def test_simplepush(self):
+        i = [1,2,3,4,5]
+        g = PushBack.PushToBack(i.__iter__())
+        g.pushback(6)
+        l = list(g)
+        assert l == [1,2,3,4,5,6]
+
+    def test_multipush(self):
+        i = [1,2,3,4,5]
+        g = PushBack.PushToBack(i.__iter__())
+        g.pushback(6)
+        assert g.next() == 1
+        g.pushback(7)
+        assert g.next() == 2
+        l = list(g)
+        assert l == [3,4,5,6,7]
+        g.pushback(8)
+        l = list(g)
+        assert l == [8]
+        l = list(g)
+        assert l == []
+
