0eaf5ae3825fcda65459b7fe5e4a618d15da45d4
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-11-12 15:43:35 +0000

    Lots of cleanups of COM wrappers and associated code * Moved `cStringIOWrapper` from `j5.Database.ADO` to `j5.Basic` * Adjusted `cStringIOWrapper` to support native `StringIO` if `cStringIO` is not present * Incorporated encoding handling from `ADO` wrapper into generic `j5.OS.COMWrapper` * Also call `COMInit.init` and `done` from `COMWrapper` * Use `COMWrapper` from `j5.Database.ADO._PyADO` * Make sure we call `COMInit.init` from `j5.Database.ADO` - needs some more checking over
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@7029 25043fe6-1616-0410-a82d-ba8f324cfe61

---
+++ j5/src/j5/Basic/cStringIOWrapper.py
@@ -0,0 +1,59 @@
+try:
+    from cStringIO import StringIO as WrappedStringIO
+except ImportError:
+    from StringIO import StringIO as WrappedStringIO
+
+class StringIO(object):
+    def __init__(self, stringio = None):
+        self.encoding = None
+        self.stringio_object = stringio
+        if self.stringio_object is None:
+            self.stringio_object = WrappedStringIO()
+    
+    def close (self):
+        return self.stringio_object.close()
+
+    def closed(self, x):
+        return self.stringio_object.closed(x)
+
+    def flush(self):
+        return self.stringio_object.flush()
+    
+    def getvalue(self, use_pos=None):
+        return self.stringio_object.getvalue(use_pos)
+
+    def isatty(self):
+        return self.stringio_object.isatty()
+
+    def next(self):
+        return self.stringio_object.next()
+
+    def read(self, s=None):
+        return self.stringio_object.read(s)
+
+    def readline(self):
+        return self.stringio_object.readline()
+
+    def readlines(self):
+        return self.stringio_object.readlines()
+
+    def reset(self):
+        return self.stringio_object.reset()
+
+    def seek(self, position):
+        return self.stringio_object.seek(position)
+
+    def softspace(self, x, base = None):
+        return self.stringio_object.softspace(x, base)
+
+    def tell(self):
+        return self.stringio_object.tell()
+
+    def truncate(self):
+        return self.stringio_object.truncate()
+
+    def write(self, s):
+        return self.stringio_object.write(s)
+
+    def writelines(self, sequence_of_strings):
+        return self.stringio_object.writelines(sequence_of_strings)
