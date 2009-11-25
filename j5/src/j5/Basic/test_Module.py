74abb0541d60501e5d532557eeffd5a4d48da074
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2009-11-25 12:45:21 +0000

    Handle running test with .pyc file
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@9531 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/test_Module.py
+++ j5/src/j5/Basic/test_Module.py
@@ -7,7 +7,9 @@ class TestModule:
     def test_find_module(self):
         target_file = Module.__file__
         if target_file.endswith(".pyc") or target_file.endswith(".pyo"):
-            target_file = target_file[:target_file.rfind(".")] + ".py"
+            source_file = target_file[:target_file.rfind(".")] + ".py"
+            if os.path.exists(source_file):
+                target_file = source_file
         assert Module.find_module("j5") == os.path.dirname(os.path.dirname(target_file))
         assert Module.find_module("j5.Basic") == os.path.dirname(target_file)
         assert Module.find_module("j5.Basic.Module") == target_file
