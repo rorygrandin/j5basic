9f525e9f34afa99b61c57a2a3bd4e2714d8a114e
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2011-09-09 15:28:03 +0000

    Switch to using a separate `interrupt_event` to get the `Timer` to re-check if something is due, and `_running` flag to say when to stop. See #6165
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@14121 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/Timer.py
+++ j5/src/j5/Basic/Timer.py
@@ -12,7 +12,8 @@ def to_seconds(timedelta):
 class Timer(object):
     """Accurate timer of resolution minimum 1 second - the idea is to guarantee accuracy"""
     def __init__(self, target, args=None, kwargs=None, resolution=1):
-        self.stop_event = threading.Event()
+        self.interrupt_event = threading.Event()
+        self._running = True
         self.target = target
         self.args = args
         if self.args is None:
@@ -26,19 +27,17 @@ class Timer(object):
             self.resolution = datetime.timedelta(seconds=resolution)
 
     def get_stop(self):
-        return self.stop_event.isSet()
+        return self._running
 
     def set_stop(self, new_stop):
-        if new_stop:
-            self.stop_event.set()
-        else:
-            self.stop_event.clear()
+        self._running = not new_stop
+        self.interrupt_event.set()
 
     stop = property(get_stop, set_stop)
 
     def start(self):
         nexttime = datetime.datetime.now()
-        while not self.stop_event.isSet():
+        while self._running:
             nexttime = nexttime + self.resolution
             currenttime = datetime.datetime.now()
             if nexttime < currenttime:
@@ -50,12 +49,13 @@ class Timer(object):
                 logging.info("Timer function missed %04d ticks between %s and %s (at %s) - running behind schedule" % (missed_count+1, first_missed_time, nexttime, currenttime))
                 nexttime += self.resolution
             else:
-                while nexttime > currenttime and not self.stop_event.isSet():
+                while nexttime > currenttime and self._running:
                     waittime = nexttime - currenttime
                     waitseconds = to_seconds(waittime)
-                    self.stop_event.wait(waitseconds)
+                    self.interrupt_event.wait(waitseconds)
+                    self.interrupt_event.clear()
                     currenttime = datetime.datetime.now()
-            if not self.stop_event.isSet():
+            if self._running:
                 apply(self.target, self.args, self.kwargs)
 
 
