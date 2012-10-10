ceaa26c328434f5f3df373131aff5dc7a1b049fd
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2012-10-10 09:26:41 +0000

    Add a decorator to set a controller to not require the database lock Protect against now_busy_op being None
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@16186 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/DatabaseWriteLock.py
+++ j5/src/j5/Basic/DatabaseWriteLock.py
@@ -10,6 +10,10 @@ database_write_lock = threading.Condition()
 thread_busy = {}
 MAX_LOCK_WAIT_TIMEOUT = 30
 
+def no_database_writes(f):
+    f.does_database_writes = False
+    return f
+
 def get_db_lock(max_wait_for_exclusive_lock=MAX_LOCK_WAIT_TIMEOUT):
     with (database_write_lock):
         current_id = threading.currentThread().ident
@@ -23,14 +27,14 @@ def get_db_lock(max_wait_for_exclusive_lock=MAX_LOCK_WAIT_TIMEOUT):
                 current_id, busy_op[0], max_wait_for_exclusive_lock)
             database_write_lock.wait(max_wait_for_exclusive_lock)
             now_busy_op = thread_busy.get(None, None)
-            if busy_op[0] == now_busy_op[0]: #same op is still busy
+            if now_busy_op and busy_op[0] == now_busy_op[0]: #same op is still busy
                 logging.error('Thread %s timed out waiting for Thread %s to release database lock ... Continuing ...',
                     current_id, busy_op[0])
                 busy_op = None
             else:
                 busy_op = now_busy_op
 
-        logging.info("Thread %s getting database lock", current_id)
+        #logging.info("Thread %s getting database lock", current_id)
         thread_busy[None] = [current_id, time.time(), 1]
 
 def release_db_lock():
@@ -41,6 +45,6 @@ def release_db_lock():
             busy_op[2] -= 1
             if busy_op[2] <= 0:
                 thread_busy.pop(None, None)
-                logging.info("Thread %s releasing database lock", current_id)
+                #logging.info("Thread %s releasing database lock", current_id)
                 database_write_lock.notify()
 
