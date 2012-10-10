ac7ff71321d61e1e6ecb48e724a5046aa1cf4a26
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2012-10-10 09:31:51 +0000

    Instrument logging in the DatabaseWriteLock method so we can test when things are logged Add a test for the warning timeout
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@16225 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/test_DatabaseWriteLock.py
+++ j5/src/j5/Basic/test_DatabaseWriteLock.py
@@ -7,13 +7,44 @@ Scenarios to test:
     * Locking a thread for warning_timeout when someone else wants it causes a warning to be logged
     * Locking a thread for max_wait_for_exclusive_lock causes an attempt at killing the offending thread
     * When a thread has the lock and two others go for it, and it stalls long enough to timeout, only one thread triggers the killing, and the second thread does not kill the one which has just done the killing
+    * Trying to get the lock while a SLAVE causes an error to be logged (but proceeds)
 """
 
 import threading
 import time
+import logging
 from j5.Test import ThreadWeave
 from j5.Basic.WithContextSkip import StatementSkipped
 from j5.Basic import DatabaseWriteLock
+
+class instrumented_logging(object):
+    def __init__(self):
+        self._loglock = threading.Lock()
+        self.clear()
+
+    def error(self, msg, *args):
+        with self._loglock:
+            self._error.append(msg % args)
+        logging.error(msg, *args)
+
+    def warning(self, msg, *args):
+        with self._loglock:
+            self._warning.append(msg % args)
+        logging.warning(msg, *args)
+
+    def info(self, msg, *args):
+        with self._loglock:
+            self._info.append(msg % args)
+        logging.info(msg, *args)
+
+    def clear(self):
+        with self._loglock:
+            self._error = []
+            self._info = []
+            self._warning = []
+
+# Override logging module
+DatabaseWriteLock.logging = instrumented_logging()
 
 class TestParallelWriteLockAccess(object):
     def do_something(self, name):
@@ -42,4 +73,29 @@ class TestParallelWriteLockAccess(object):
         for thread in threads:
             thread.join()
         assert expected_names == self.run
+
+class TestWarningTimeout(object):
+    def do_something_for_awhile(self):
+        with ThreadWeave.only_thread('second_thread') as StatementSkipped.detector:
+            # Make sure the first thread gets the lock first
+            time.sleep(0.5)
+        DatabaseWriteLock.get_db_lock(warning_timeout=2)
+        try:
+            with ThreadWeave.only_thread('first_thread') as StatementSkipped.detector:
+                time.sleep(3)
+        finally:
+            DatabaseWriteLock.release_db_lock()
+
+    def test_warning_timeout(self):
+        DatabaseWriteLock.logging.clear()
+        class server:
+            mode = DatabaseWriteLock.Admin.ServerModeEnum.SINGLE
+        DatabaseWriteLock.ServerMode().server = server
+        first_thread = threading.Thread(target=self.do_something_for_awhile, name="first_thread")
+        second_thread = threading.Thread(target=self.do_something_for_awhile, name="second_thread")
+        first_thread.start()
+        second_thread.start()
+        first_thread.join()
+        second_thread.join()
+        assert ("Thread %s still waiting for database lock after 2s - this may timeout" % second_thread.ident) in DatabaseWriteLock.logging._warning
 
