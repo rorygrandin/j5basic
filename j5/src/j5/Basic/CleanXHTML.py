ff322dff371c4eb82480cd820ca43b7b93b2c3ba
matth <matth@25043fe6-1616-0410-a82d-ba8f324cfe61>
2013-02-14 12:26:17 +0000

    Previously, the cleaning of rich text would result in extra tags and things getting added (e.g. an extra div around all of the content, if there wasn't a single containing node, and extra <p> tags around plain text strings). This changes the implementation to wrap the content in a div, before passing it to lxml to clean, and then unwrapping again afterwards. Also removed support for cleaning xml objects instead of html fragments.
    And then added some tests.
    
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@16808 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/CleanXHTML.py
+++ j5/src/j5/Basic/CleanXHTML.py
@@ -1,29 +1,31 @@
 import copy
+import xml
 from lxml.html import clean
 from lxml.html import tostring, fromstring
 
-
-def _transform_result(typ, result):
-    """Convert the result back into the input type.
-    """
-    if issubclass(typ, str):
-        return tostring(result, encoding = 'utf-8', method = 'xml')
-    elif issubclass(typ, unicode):
-        return tostring(result, encoding = unicode, method = 'xml')
-    else:
-        return result
-
-
-
 class Cleaner(clean.Cleaner):
     def clean_html(self, html):
-        result_type = type(html)
-        if isinstance(html, basestring):
-            doc = fromstring(html)
-        else:
-            doc = copy.deepcopy(html)
-        self(doc)
-        return _transform_result(result_type, doc)
+        if not html:
+            return html
+        if not html.strip():
+            return html
+        if not isinstance(html, basestring):
+            raise ValueError('We only support cleaning HTML fragments')
+        encode = False
+        if not isinstance(html, unicode):
+            html = html.decode('utf-8')
+            encode = True
+
+        #We wrap the content up in an extra div tag (otherwise lxml does wierd things to it - like adding in <p> tags and stuff)
+        divnode = fromstring(u'<div>' + html + u'</div>')
+        self(divnode)
+        #Now unwrap it (i.e. just serialize the children of our extra div node:
+        cleaned = xml.sax.saxutils.escape(divnode.text) if divnode.text else ''
+        for n in divnode:
+            cleaned += tostring(n, encoding = unicode, method = 'xml')
+        if encode:
+            cleaned = cleaned.encode('utf-8')
+        return cleaned
 
 cleaner = Cleaner()
 clean_html = cleaner.clean_html
