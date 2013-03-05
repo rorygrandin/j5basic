e23afab1bcd7b359b3473f13d8d356e133846653
roryg <roryg@25043fe6-1616-0410-a82d-ba8f324cfe61>
2013-03-05 07:29:03 +0000

    Update to handle cleaning in one pass, and so that we don't completely strip out style attributes. (See #8984)
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@16950 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Basic/CleanXHTML.py
+++ j5/src/j5/Basic/CleanXHTML.py
@@ -1,8 +1,9 @@
-import copy
 import cssutils
 from xml.sax import saxutils
 from lxml.html import tostring, fromstring, clean
 from lxml import etree
+
+import logging
 
 class Cleaner(clean.Cleaner):
     def clean_html(self, html):
@@ -12,47 +13,42 @@ class Cleaner(clean.Cleaner):
         #We wrap the content up in an extra div tag (otherwise lxml does wierd things to it - like adding in <p> tags and stuff)
         divnode = fromstring(u'<div>' + html + u'</div>')
         self(divnode)
-        #Now unwrap it (i.e. just serialize the children of our extra div node:
+
+        # Strip all class attributes
+        etree.strip_attributes(divnode, 'class')
+
+        for style in divnode.xpath("//@style"):
+            parent = style.getparent()
+            try:
+                cssStyle = cssutils.parseStyle(style)
+            except Exception, e:
+                logging.info("Style %s failed to parse with error %s." % (style, e))
+                parent.attrib.pop('style', None)
+                continue
+
+            # Set the line separator so that the style gets serialized
+            cssutils.ser.prefs.lineSeparator = ''
+            # Only allow valid style properties
+            cssutils.ser.prefs.validOnly = True
+
+            new_style = cssStyle.cssText
+            if not new_style.strip():
+                parent.attrib.pop('style', None)
+            else:
+                parent.attrib['style'] = new_style
+
+        # Drop all empty span tags
+        for span_tag in divnode.xpath("//span"):
+            if not span_tag.keys():
+                span_tag.drop_tag()
+
+        #Now unwrap the divnode (i.e. just serialize the children of our extra div node)
         cleaned = saxutils.escape(divnode.text) if divnode.text else ''
+
         for n in divnode:
             cleaned += tostring(n, encoding = unicode, method = 'xml')
         return cleaned
 
-def clean_tags(html):
-    """clean assumes you tidied first..."""
-    root = etree.fromstring("<div>" + html + "</div>")
-    for cls in etree.XPath("//@class")(root):
-        parent = cls.getparent()
-        classes = [cls for cls in cls.split() if not cls.lower().startswith("mso")]
-        if not classes:
-            parent.attrib.pop("class", None)
-        else:
-            parent.attrib["class"] = " ".join(classes)
-    for c in root.xpath('//comment()'):
-        p = c.getparent()
-        p.remove(c)
-    for style in etree.XPath("//@style")(root):
-        parent = style.getparent()
-        cssStyle = cssutils.parseStyle(style)
-
-        #Strip all style elements that originate from MS
-        for key in cssStyle.keys():
-            if key.startswith('mso'):
-                del cssStyle[key]
-
-        new_style = cssStyle.cssText
-        if not new_style.strip():
-            parent.attrib.pop("style", None)
-        else:
-            parent.attrib["style"] = new_style
-
-    #Now unwrap it (i.e. just serialize the children of our extra div node:
-    cleaned = saxutils.escape(root.text) if root.text else ''
-
-    for n in root:
-        cleaned += etree.tostring(n, encoding = unicode, method = 'xml')
-
-    return cleaned
-
-cleaner = Cleaner()
+# We need safe_attrs_only set to False, otherwise it strips out style attributes completely
+cleaner = Cleaner(safe_attrs_only=False)
 clean_html = cleaner.clean_html
