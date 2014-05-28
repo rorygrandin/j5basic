faa9b60dccc3f8b5fb531e5d4962251dd85845c3
matth <matth@sjsoft.com>
2014-05-28 11:34:01 +0200

    We seem to be using the Apache license.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='j5.Basic',
     version='1.0',
     packages=['j5', 'j5.Basic'],
-    license='MIT License',
+    license='Apache Software License',
     description='A collection of utility methods and classes.',
     long_description=open('README.md').read(),
     url='http://www.j5int.com/',
@@ -12,7 +12,7 @@ setup(
     author_email='support@sjsoft.com',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
