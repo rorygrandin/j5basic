23fc9cf0e14428f37110b594a96b08fef8308fc0
matth <matth@sjsoft.com>
2014-05-28 09:51:31 +0200

    Adding a setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='j5.Basic',
+    version='1.0',
+    packages=['j5', 'j5.Basic'],
+    license='MIT License',
+    description='A collection of utility methods and classes.',
+    long_description=open('README.md').read(),
+    url='http://www.j5int.com/',
+    author='j5 Software',
+    author_email='support@sjsoft.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
