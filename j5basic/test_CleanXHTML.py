db220df54ac2d95c19947a972ea5f15711184a06
Dee Moore <deem@j5int.com>
2019-02-07 13:25:12 +0200

    Add a test for cleanxhtml

---
+++ j5basic/test_CleanXHTML.py
@@ -0,0 +1,12 @@
+
+from j5basic import CleanXHTML
+
+DIRTY_XHTML = """
+Testing fragment<br/>
+<span id="this_is_a_great_id" class="MYCLASS" style="badstyle" lang="en-inve-us">TEXT</span>
+<span/>
+"""
+
+def test_clean_html():
+    cleaned_html = CleanXHTML.clean_html(DIRTY_XHTML)
+    assert cleaned_html == "Testing Fragment<br/>\n<span>TEXT</span>\n"
