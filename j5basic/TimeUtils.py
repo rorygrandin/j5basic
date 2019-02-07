315a7d747e344e4714c9ddd5d7e8e276abe69e76
Dee Moore <deem@j5int.com>
2019-02-07 12:57:50 +0200

    We need to check if we need the patch in the underlying datetime type, or we can run into issues with import order

--- j5basic/TimeUtils.py
+++ j5basic/TimeUtils.py
@@ -1,5 +1,5 @@
 from j5basic import TzInfo
-from virtualtime import datetime_tz
+from virtualtime import datetime_tz, _underlying_datetime_type
 import datetime
 import threading
 import time
@@ -113,7 +113,7 @@ def _findall(text, substr):
 
 _NEEDS_STRFTIME_PATCH = True
 try:
-    datetime.datetime(1,1,1).strftime("%Y-%m-%d")
+    _underlying_datetime_type(1,1,1).strftime("%Y-%m-%d")
     _NEEDS_STRFTIME_PATCH = False
 except ValueError:
     pass
