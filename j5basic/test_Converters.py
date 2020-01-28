f355b13cca4b58f9d3e742afa9da700be1f6c399
Yaseen Mowzer <yaseenm@j5int.com>
2020-01-28 17:47:31 +0200

    Add back test_Converters.py

---
+++ j5basic/test_Converters.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+from j5basic import Converters
+from j5test.Utils import raises
+
+def test_temperature_converter():
+    temp_conv = Converters.TemperatureConverter()
+    assert temp_conv.string_to_units('degC') == Converters.TemperatureConverter.CELSIUS
+    assert temp_conv.convert_from(32, temp_conv.string_to_units('degF')) == 0
+    assert temp_conv.convert_to(0, temp_conv.string_to_units('degF')) == 32
+
+    assert raises(NotImplementedError, temp_conv.convert_from, 32, 'degF')
+    assert raises(NotImplementedError, temp_conv.convert_to, 0, 'degC')
+
+def test_pressure_converter():
+    pc = Converters.PressureConverter()
+    assert pc.convert_from(12, pc.KGCM2) is not None
+
+
+
